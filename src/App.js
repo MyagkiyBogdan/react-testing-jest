dea9da337e03b058936a9fe9aa98df8465aa392a
BogdanMyagkiy <myagkiybogdan@gmail.com>
2022-07-26 20:02:23 +0300

    added react test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ function App() {
     <div className="App">
       <h1>Hello World</h1>
       <button>Click me</button>
-      <input type="text" />
+      <input type="text" placeholder="name" />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('render App test', () => {
-  render(<App />);
-  const helloWorldElem = screen.getByText(/hello world/i); // регулярное выражения для поиска текста с игнорированием регистра
-  expect(helloWorldElem).toBeInTheDocument();
+describe('App', () => {
+  test('render App test', () => {
+    render(<App />);
+    const helloWorldElem = screen.getByText(/hello world/i); // регулярное выражения для поиска текста с игнорированием регистра
+    const btn = screen.getByRole('button');
+    const input = screen.getByPlaceholderText(/name/i);
+    expect(helloWorldElem).toBeInTheDocument();
+    expect(btn).toBeInTheDocument();
+    expect(input).toBeInTheDocument();
+    // screen.debug();
+    expect(input).toMatchSnapshot();
+  });
 });
