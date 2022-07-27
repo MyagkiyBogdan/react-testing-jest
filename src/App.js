44017e190f3dd7977cc349709bdfc24f79cb7d79
BogdanMyagkiy <myagkiybogdan@gmail.com>
2022-07-27 11:23:28 +0300

    more test added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { useEffect, useState } from 'react';
+
 function App() {
+  const [data, setData] = useState(null);
+  const [toggle, setToggle] = useState(false);
+
+  const handleClick = () => setToggle(prevValue => !prevValue);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setData({ name: 'Jhon' });
+    }, 100);
+  }, []);
+
   return (
     <div className="App">
+      {toggle && <div data-testid="toggle-elem">Toggle!</div>}
+      {data && <div style={{ color: 'red' }}>{data.name}</div>}
       <h1>Hello World</h1>
-      <button>Click me</button>
+      <button type="button" onClick={handleClick} data-testid="toggle-btn">
+        Click me
+      </button>
       <input type="text" placeholder="name" />
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -13,4 +13,22 @@ describe('App', () => {
     // screen.debug();
     expect(input).toMatchSnapshot();
   });
+
+  // findBy - асинхронный код
+  // getBy - синхронный, точно есть в документе
+  // queryBy - проверить что бы доказать что элемента нету, не гарантирует 100%
+
+  test('render App not in document test', () => {
+    render(<App />);
+    const notInText = screen.queryByText(/not in text/i);
+    expect(notInText).toBeNull();
+  });
+
+  test('render async data', async () => {
+    render(<App />);
+    screen.debug();
+    const asyncData = await screen.findByText(/jhon/i);
+    expect(asyncData).toBeInTheDocument();
+    expect(asyncData).toHaveStyle({ color: 'red' });
+  });
 });
